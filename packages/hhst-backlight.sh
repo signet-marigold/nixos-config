62751ceb68717dc5de09cd9fd3251e6028905b0f
Alex Hack <perad@sqwu.me>
2024-07-24 18:38:34 -0500

    fixed bug in backlight script

--- packages/hhst-backlight.sh
+++ packages/hhst-backlight.sh
@@ -2,16 +2,25 @@ MSGTAG="Backlight"
 CHANGE="5"
 SOUNDPATH=""
 
+LIGHT=$( xbacklight -get )
+
 case $1 in
   -i)
     xbacklight -perceived -fps 30 -inc ${CHANGE}
     ;;
   -d)
-    xbacklight -perceived -fps 30 -dec ${CHANGE}
+    if [[ $LIGHT > 1 ]]; then
+      xbacklight -perceived -fps 30 -dec ${CHANGE}
+    fi
     ;;
 esac
 
 LIGHT=$( xbacklight -get )
+
+if [[ $LIGHT < 1 ]]; then
+  xbacklight -set 1
+  LIGHT=1
+fi
 
 # Show the volume notification
 dunstify -a "changeBacklight" -u low -h string:x-dunst-stack-tag:$MSGTAG \
