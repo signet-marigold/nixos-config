0f9f6eac5561ae967bb4291e32a99880a7464250
Alex Hack <perad@sqwu.me>
2024-08-30 00:00:23 -0500

    minified slstatus prompt on both machines

--- hosts/AT08-OBAMA/modules/slstatus/slstatus-config.h
+++ hosts/AT08-OBAMA/modules/slstatus/slstatus-config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 100;
+const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "#";
@@ -65,9 +65,9 @@ static const char unknown_str[] = "#";
  */
 static const struct arg args[] = {
   /* function          format              argument */
-  { netspeed_rx,       "⬇%s",              "enp5s0" },
-  { netspeed_tx,       " ⬆%s ",            "enp5s0" },
-  { cpu_perc,          " CPU %s%% ",       NULL },
-  { ram_perc,          " RAM %s%% ",       NULL },
+/*  { netspeed_rx,       "⬇%s",              "enp5s0" },*/
+/*  { netspeed_tx,       " ⬆%s ",            "enp5s0" },*/
+/*  { cpu_perc,          " CPU %s%% ",       NULL },*/
+/*  { ram_perc,          " RAM %s%% ",       NULL },*/
   { datetime,          " %s ",             "%F %T" },
 };
