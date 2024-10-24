f730069a94968a6b411e5e52f8fb784fea993551
Alex Hack <perad@sqwu.me>
2024-10-23 20:45:45 -0500

    switched back to english

--- modules/base/internationalisation.nix
+++ modules/base/internationalisation.nix
@@ -13,12 +13,12 @@
   
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
-    "es_US.UTF-8/UTF-8"
+    #"es_US.UTF-8/UTF-8"
     #"uk_UA.UTF-8/UTF-8"
     #"ru_RU.UTF-8/UTF-8"
   ];
   
-  i18n.defaultLocale = "es_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
