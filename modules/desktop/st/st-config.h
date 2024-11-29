343dca758df7aee79d09d11f461b0828866486fa
Alex Hack <perad@sqwu.me>
2024-11-29 00:46:15 -0600

    extended st border to 12 pixels

--- modules/desktop/st/st-config.h
+++ modules/desktop/st/st-config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 4;
+static int borderpx = 12;
 
 /*
  * What program is execed by st depends of these precedence rules:
