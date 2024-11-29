97765a8adb1d1c226ba85a95d0eea57de10cbb2b
Alex Hack <perad@sqwu.me>
2024-11-29 15:21:02 -0600

    tweeked picom again

--- modules/desktop/targets/dwm-desktop/default.nix
+++ modules/desktop/targets/dwm-desktop/default.nix
@@ -2,20 +2,28 @@
   # A compositor for transparency and other desktop effects
   services.picom = {
     enable = true;
-    backend = "glx";
-    vSync = true;
+    # Fading settings
     fade = true;
-    fadeDelta = 2;
+    # on 60hz displays it doesn't make sense to have this faster than 16ms
+    # hoping there's interpil
+    fadeDelta = 10; # time (in ms) between frames
+    fadeSteps = [ (0.15) (0.17) ]; # in, out
     fadeExclude = [
       "class_g = 'xsecurelock'"
     ];
+    # Shadow settings
     shadow = true;
-    shadowOpacity = 0.35;
-    shadowOffsets = [ (-15) (-15) ];
+    shadowOpacity = 0.25;
+    shadowOffsets = [ (-15) (-15) ]; # X, Y
     shadowExclude = [
       "name = 'cpt_frame_xcb_window'"
       "class_g ?= 'zoom'"
+      "class_g = 'dwm'"
     ];
+    # General settings
+    backend = "glx";
+    vSync = true;
+    menuOpacity = 0.88; # For reference: dmenu's opacity is 0.8784 percent
     wintypes = {
       # popup_menu = { opacity = config.services.picom.menuOpacity; };
       # dropdown_menu = { opacity = config.services.picom.menuOpacity; };
