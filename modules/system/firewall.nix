df46d55dc2da1df507dbe0e03ff06f0a63d41047
Alex Hack <perad@sqwu.me>
2024-12-10 20:45:20 -0600

    removed remote access to syncthing gui

--- modules/system/firewall.nix
+++ modules/system/firewall.nix
@@ -9,6 +9,6 @@
   # source: https://docs.syncthing.net/users/firewall.html
 
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 7590 8384 22000 ];
+  networking.firewall.allowedTCPPorts = [ 7590 22000 ];
   networking.firewall.allowedUDPPorts = [ 1194 1198 1500 22000 21027 ];
 }
