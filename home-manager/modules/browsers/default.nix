b4c2048bdb5b484a1268ba75c96f9d0ddaf33dc1
Alex Hack <perad@sqwu.me>
2025-03-29 12:42:21 -0500

    disable a few browser

--- home-manager/modules/browsers/default.nix
+++ home-manager/modules/browsers/default.nix
@@ -1,9 +1,9 @@
 {
   imports = [
-    ./firefox.nix
-    ./librewolf.nix
-    ./qutebrowser.nix
-    ./mullvad-browser.nix
+    #./firefox.nix
+    #./librewolf.nix
+    #./qutebrowser.nix
+    #./mullvad-browser.nix
     ./tor-browser.nix
     ./chrome.nix
     ./zen-browser.nix
